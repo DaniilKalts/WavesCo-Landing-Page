406f931d45dfb88effebce248e2a69ba11315f0f
Daniil Kalts <kaltsdaniil@gmail.com>
2024-04-13 12:16:47 +0500

    11 feat: add customers section

---
+++ src/scripts/swiper.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  spaceBetween: 30,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    525: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
